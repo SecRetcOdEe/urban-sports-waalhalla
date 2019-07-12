b01aaa9aea93860628f17bd8502885c4aea38c53
Nelson <Justanotherdayonline@tutanota.com>
2019-07-12 16:25:55 +0200

    All files for urban-sports-waalhalla repo are commited

---
+++ gulp/tasks/build.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp'),
+imagemin = require('gulp-imagemin'),
+del = require('del'),
+usemin = require('gulp-usemin'),
+rev = require('gulp-rev'),
+cssnano = require('gulp-cssnano'),
+uglify = require('gulp-uglify'),
+browserSync = require('browser-sync');
+
+gulp.task('previewDocs', function() {
+  browserSync.init({
+    server: {
+    baseDir: "docs"
+    }
+  });
+});
+
+gulp.task('deleteDocsFolder', function() {
+  return del('./docs');
+});
+
+gulp.task('optimizeImages', ['deleteDocsFolder'], function() {
+  return gulp.src(['./app/assets/images/**/*', '!./app/assets/images/icons', '!./app/assets/images/icons/**/*'])
+   .pipe(imagemin({
+     progressive: true,
+     interlaced: true,
+     multipass: true
+   }))
+   .pipe(gulp.dest("./docs/assets/images"));
+});
+
+gulp.task('usemin', ['deleteDocsFolder'], function() {
+  return gulp.src("./app/index.html")
+   .pipe(usemin({
+     css: [function() {return rev()}, function() {return cssnano()}],
+     js: [function() {return rev()}, function() {return uglify()}]
+   }))
+   .pipe(gulp.dest("./docs"));
+});
+
+gulp.task('build',['deleteDocsFolder', 'optimizeImages', 'usemin']);
